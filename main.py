bc9bf51257a7d4825fa1803fcc69fa2194d2a1b9
SimonKragh <simonkraghandersen@gmail.com>
2025-03-28 22:11:53 +0100

    added print report

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
-from stats import get_num_words
+from stats import get_num_words, get_number_of_characters, get_chars_by_frequency
 
 def get_book_text(file_path: str) -> str:
     with open(file_path, "r") as file:
         return file.read()
 
 def main():
-    num_words = get_num_words(get_book_text("books/frankenstein.txt"))
-    print(f"{num_words} words found in the document ")
+    file_path = "books/frankenstein.txt"
+    text = get_book_text(file_path)
 
+    num_words = get_num_words(text)
+    character_counts = get_number_of_characters(text)
+
+
+    #Print the report 
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}...")
+    print(f"----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print(f"--------- Character Count -------")
+
+    sorted_characters = get_chars_by_frequency(character_counts)
+    for char, count in sorted_characters:
+        print(f"{char}: {count}")
+
+    print("============= END ===============")
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -1,2 +1,23 @@
-    def number_of_words(text: str) -> int:
+def get_num_words(text: str) -> int:
     return len(text.split())
+
+def get_number_of_characters(text: str) -> dict[str, int]:
+    char_count = {}
+    for char in text.lower():
+        if char.isalpha():
+            if char not in char_count:
+                char_count[char] = 0
+            char_count[char] += 1
+    return char_count
+
+def get_chars_by_frequency(char_count: dict[str, int]) -> list[dict[str, int]]:
+    """
+    Returns a list of characters sorted by their frequency (most frequent first).
+    
+    Args:
+        text: The input text to analyze
+        
+    Returns:
+        A list of tuples (character, count) sorted by count in descending order
+    """
+    return sorted(char_count.items(), key=lambda x: x[1], reverse=True)
