8caebbad90eca0bce9ac2d975a317c5c6fef8a11
SimonKragh <simonkraghandersen@gmail.com>
2025-03-28 22:20:07 +0100

    project ready for submission

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from stats import get_num_words, get_number_of_characters, get_chars_by_frequency
+import sys 
 
 def get_book_text(file_path: str) -> str:
     with open(file_path, "r") as file:
         return file.read()
 
 def main():
-    file_path = "books/frankenstein.txt"
-    text = get_book_text(file_path)
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
+    text = get_book_text(book_path)
 
     num_words = get_num_words(text)
     character_counts = get_number_of_characters(text)
@@ -14,7 +19,7 @@ def main():
 
     #Print the report 
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {file_path}...")
+    print(f"Analyzing book found at {book_path}...")
     print(f"----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print(f"--------- Character Count -------")
